baac8dc8b3e542fe6ffd08d94aba5d45cc446654
GleidsonSousa <gleidson2090@gmail.com>
2023-05-05 11:20:48 -0300

    new

---
+++ docs/importacao.sql
@@ -0,0 +1,32 @@
+LOAD DATA INFILE "C:/Users/Desenvolvimento/Desktop/GITHUB/techman/docs/dados/perfis.csv"
+INTO TABLE perfil
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE "C:/Users/Desenvolvimento/Desktop/GITHUB/techman/docs/dados/usuarios.csv"
+INTO TABLE usuario
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE "C:/Users/Desenvolvimento/Desktop/GITHUB/techman/docs/dados/equipamentos.csv"
+INTO TABLE equipamento
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE "C:/Users/Desenvolvimento/Desktop/GITHUB/techman/docs/dados/comentarios.csv"
+INTO TABLE comentario
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+select * from perfil
+select * from usuario
+select * from equipamento
+select * from comentario
